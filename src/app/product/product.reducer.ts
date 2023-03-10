fbe550139ee567059e1870ffc04ed79eb1af7ebe
daniel.granados <daniel.granados@sofka.com.co>
2023-03-10 11:24:37 -0500

    Upd: product html

--- src/app/product/product.reducer.ts
+++ src/app/product/product.reducer.ts
@@ -21,18 +21,11 @@ export const initialState: ProductState = {
 
 export const productReducer = createReducer(
   initialState,
-  on(ProductActions.loadProducts, (state) => {
-    console.debug('State load', state);
-    return {
-      ...state,
-    };
-  }),
-  on(ProductActions.productsLoadedSuccess, (state, { payload }) => {
-    console.debug('State loaded', state);
-    console.debug('Products', payload);
-    return {
-      ...state,
-      products: payload,
-    };
-  })
+  on(ProductActions.loadProducts, (state) => ({
+    ...state,
+  })),
+  on(ProductActions.productsLoadedSuccess, (state, { payload }) => ({
+    ...state,
+    products: payload,
+  }))
 );
